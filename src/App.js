8e488e71d612d6327e78751d13986eaf9185c697
nm1008 <mallari08nikko@yahoo.com>
2023-09-13 08:58:24 +0800

    added input field

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  console.log(input);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>classy weather</h1>
+      <Input setInput={setInput} input={input} />
     </div>
+  );
+}
+
+function Input({ input, setInput }) {
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="Search from location"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+    </>
   );
 }
 
