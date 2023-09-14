ed259c1f35ddc6981ff9c686cdf0b60dd6303f60
nm1008 <mallari08nikko@yahoo.com>
2023-09-15 07:40:29 +0800

    added fetch api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [input, setInput] = useState("");
-  console.log(input);
+  const [location, setLocation] = useState("");
+
+  useEffect(() => {
+    async function fetchWeather() {
+      try {
+        const geoRes = await fetch(
+          `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
+        );
+        if (!geoRes.ok)
+          throw new Error("Something went wrong with fetching weather");
+
+        const geoData = await geoRes.json();
+        console.log(geoData.results);
+        if(!geoData.results) throw new Error("Location not found")
+
+
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    if (location.length > 3) {
+      fetchWeather();
+    }
+  }, [location]);
 
   return (
-    <div className="App">
-      <h1>classy weather</h1>
-      <Input setInput={setInput} input={input} />
+    <div className="app">
+      <h1>Classy Weather</h1>
+      <Input location={location} setLocation={setLocation} />
     </div>
   );
 }
 
-function Input({ input, setInput }) {
+function Input({ location, setLocation }) {
   return (
     <>
       <input
         type="text"
-        placeholder="Search from location"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
+        placeholder="Enter country"
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
       />
     </>
   );
