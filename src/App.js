52b854a504fc9a188da8fe8cced4dc85837130d7
nm1008 <mallari08nikko@yahoo.com>
2023-09-15 07:53:34 +0800

    added fetch request

--- src/App.js
+++ src/App.js
@@ -2,28 +2,36 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [location, setLocation] = useState("");
+  const [weather, setWeather] = useState({});
+  const [displayLocation, setDisplayLocation] = useState("");
 
- 
-    const getWeather = async function fetchWeather() {
-      try {
-        const geoRes = await fetch(
-          `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
-        );
-        if (!geoRes.ok)
-          throw new Error("Something went wrong with fetching weather");
+  const getWeather = async function fetchWeather() {
+    try {
+      const geoRes = await fetch(
+        `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
+      );
+      if (!geoRes.ok)
+        throw new Error("Something went wrong with fetching weather");
 
-        const geoData = await geoRes.json();
-        console.log(geoData.results);
-        if(!geoData.results) throw new Error("Location not found")
-          
+      const geoData = await geoRes.json();
+      // console.log(geoData.results);
+      if (!geoData.results) throw new Error("Location not found");
 
+      //getting the data from the fetch request only the first index
+      const { latitude, longitude, timezone, name } = geoData.results.at(0);
 
-      } catch (err) {
-        console.log(err);
-      }
+      setDisplayLocation(name);
+
+      const weatherRes = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
+      );
+      const weatherData = await weatherRes.json();
+      setWeather(weatherData.daily);
+    } catch (err) {
+      console.log(err);
     }
-  
-  
+  };
+
   return (
     <div className="app">
       <h1>Classy Weather</h1>
