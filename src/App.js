2bd38a07dcc66f63f2c02703f847dd88837e8dbf
nm1008 <mallari08nikko@yahoo.com>
2023-09-15 08:13:18 +0800

    added 2 functions / getWeatherIcon and formatDay

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+
+function getWeatherIcon(wmoCode) {
+  const icons = new Map([
+    [[0], "☀️"],
+    [[1], "🌤"],
+    [[2], "⛅️"],
+    [[3], "☁️"],
+    [[45, 48], "🌫"],
+    [[51, 56, 61, 66, 80], "🌦"],
+    [[53, 55, 63, 65, 57, 67, 81, 82], "🌧"],
+    [[71, 73, 75, 77, 85, 86], "🌨"],
+    [[95], "🌩"],
+    [[96, 99], "⛈"],
+  ]);
+  const arr = [...icons.keys()].find((key) => key.includes(wmoCode));
+  if (!arr) return "NOT FOUND";
+  return icons.get(arr);
+}
 
 function App() {
   const [location, setLocation] = useState("");
   const [weather, setWeather] = useState({});
   const [displayLocation, setDisplayLocation] = useState("");
+  const [isLoading, setIsLoading] = useState(false)
 
   const getWeather = async function fetchWeather() {
     try {
+      setIsLoading(true)
       const geoRes = await fetch(
         `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
       );
@@ -37,6 +57,7 @@ function App() {
       <h1>Classy Weather</h1>
       <Input location={location} setLocation={setLocation} />
       <button onClick={getWeather}>Get Weather</button>
+      <Weather displayLocation={displayLocation} isLoading={isLoading} weather={weather}/>
     </div>
   );
 }
@@ -53,5 +74,43 @@ function Input({ location, setLocation }) {
     </>
   );
 }
+
+function Weather({ displayLocation, isLoading, weather }) {
+
+  const {
+    temperature_2m_max: max,
+    temperature_2m_min: min,
+    time: dates,
+    weathercode: codes,
+  } = weather;
+
+
+  return (
+    <>
+    {isLoading && 
+    <h2>Weather {displayLocation}</h2>}
+    <ul className="weather">
+      {dates.map((date, i) => (
+        console.log(date)
+      ))}
+    </ul>
+    </>
+  )
+}
+
+function Day({ date, max, min, code, isToday }) {
+  return (
+    <>
+      <li className="day">
+        <span>{getWeatherIcon(code)}</span>
+        <p>{isToday ? "Today" : formatDay(date)}</p>
+        <p>
+          {Math.floor(min)}&deg; &mdash; <strong>{Math.ceil(max)}&deg;</strong>
+        </p>
+      </li>
+    </>
+  );
+}
+
 
 export default App;
